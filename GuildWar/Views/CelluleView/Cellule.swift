6be1d60e0a7b8446c580ce298449af265dbeb4c3
William Tomas <william.tomas@mobiapps.fr>
2019-06-05 14:53:01 +0200

    Squelette créé

---
+++ GuildWar/Views/CelluleView/Cellule.swift
@@ -0,0 +1,9 @@
+//
+//  Cellule.swift
+//  GuildWar
+//
+//  Created by William Tomas on 05/06/2019.
+//  Copyright © 2019 William Tomas. All rights reserved.
+//
+
+import Foundation
---
+++ GuildWar/src/API/Api.swift
@@ -0,0 +1,12 @@
+//
+//  Api.swift
+//  GuildWar
+//
+//  Created by William Tomas on 05/06/2019.
+//  Copyright © 2019 William Tomas. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+//ici on gère les appels vers l'API
