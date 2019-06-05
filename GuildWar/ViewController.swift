42a559332973ece2d1696a0b87bc8e3572b551b7
William Tomas <william.tomas@mobiapps.fr>
2019-06-05 14:30:50 +0200

    premier commit

---
+++ GuildWar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuildWar
+//
+//  Created by William Tomas on 05/06/2019.
+//  Copyright © 2019 William Tomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
